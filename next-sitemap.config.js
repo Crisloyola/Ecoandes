bebf04f533e20ef8e88e9f1b86ad15f04de31a3a
cristopher <loyoladv@gmail.com>
2025-09-23 18:38:58 -0500

    actualizacion SEO

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,4 +1,8 @@
+/** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: "https://www.logisticaecoandes.com/",
-  generateRobotsTxt: true,
+  siteUrl: "https://www.logisticaecoandes.com", // sin slash final
+  generateRobotsTxt: true, // genera robots.txt automáticamente
+  sitemapSize: 5000, // opcional, por si tuvieras muchísimas URLs
+  changefreq: "weekly", // sugerencia para Google
+  priority: 0.7,        // prioridad por defecto
 };
